7612d838e0baebd527093978c5b5eefdd5efd0d6
Sourabh Mandal <sourabh_mandal@persistent.com>
2016-09-15 13:16:07 +0530

    Commit _ 1 _ Maven Build _ Jenkins Enabled

---
+++ src/main/java/com/psl/App.java
@@ -0,0 +1,13 @@
+package com.psl;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello This is a Maven First Job Example, To Be Deployed with Jenkins over git" );
+    }
+}
